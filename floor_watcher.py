cb1cd3bf0752dba2b17bfd747dd4922fe0f4a05e
mustafa babar <mustafababar7@gmail.com>
2022-06-05 15:34:07 +0100

    finished basic logic for magic eden nft floor watcher

--- floor_watcher.py
+++ floor_watcher.py
@@ -2,44 +2,45 @@ import requests
 import threading
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-def magic_eden(collectionName,floorprice,user='329651231272337418'):
-    prev = 0
-    s = False
-    while not s:
-        url = f"https://api-mainnet.magiceden.dev/v2/collections/{collectionName}/stats"
+def webhooks(user,collectionName,floorprice,floorP):
+        mWebhook = DiscordWebhook(
+            url='https://discord.com/api/webhooks/814320851930841109/PzSxpUmTSN46nCvEhHMziNmVc6-pFoNYCVIaSfWhSbPclB-bjDnYNrhVIYR9uNU0NfSF',
+            rate_limit_retry=True,
+            content = f"<@{user}>",
+            allowed_mentions = {'users': [user]}
+            )
+            
+        embed = DiscordEmbed(
+            title = "Alert FP Hit",
+            description = f"[{collectionName}](https://magiceden.io/marketplace/{collectionName})",
+            color = 6710937
+            )
+        embed.add_embed_field(name='Collection',value=collectionName,inline = True)
+        embed.add_embed_field(name='Floor Price To be alerted at',value=str(floorprice),inline = True)
+        embed.add_embed_field(name='Current Floor Price',value=str(floorP),inline = True)
+        mWebhook.add_embed(embed)
+        mWebhook.execute()
 
+def magic_eden(collectionName,floorprice,user='329651231272337418'):
+    prev = None
+    url = f"https://api-mainnet.magiceden.dev/v2/collections/{collectionName}/stats"
+    final = 0
+    while True:
         payload={}
         headers = {}
 
         response = requests.get(url, headers=headers, data=payload)
         floorP = float(response.json()['floorPrice']/1000000000)
-        prev = floorP
-
-        if floorprice == floorP or (floorP>prev and floorprice<floorP) or (prev>floorP and floorP>floorprice):
-            mWebhook = DiscordWebhook(
-                url='https://discord.com/api/webhooks/814320851930841109/PzSxpUmTSN46nCvEhHMziNmVc6-pFoNYCVIaSfWhSbPclB-bjDnYNrhVIYR9uNU0NfSF',
-                rate_limit_retry=True,
-                content = f"<@{user}>",
-                allowed_mentions = {'users': [user]}
-                )
-                
-            embed = DiscordEmbed(
-                title = "Alert FP Hit",
-                description = f"[{collectionName}](https://magiceden.io/marketplace/{collectionName})",
-                color = 6710937
-                )
-            embed.add_embed_field(name='Collection',value=collectionName,inline = True)
-            embed.add_embed_field(name='Floor Price To be alerted at',value=str(floorprice),inline = True)
-            embed.add_embed_field(name='Current Floor Price',value=str(floorP),inline = True)
-            mWebhook.add_embed(embed)
-            mWebhook.execute()
-            print("True")
-            s = True
-            return ""
-
         
-finished = False
+        if prev == None:
+            prev=floorP
 
+        if floorprice == floorP or (floorP>prev and floorprice<floorP) or (prev>floorP and floorP<floorprice):
+            webhooks(user,collectionName,floorprice,floorP)
+            return None
+        prev=floorP
+
+finished = False
 while not finished:
     collection = input("Enter Collection: ")
     floor = float(input("Enter floor price to get alert: "))
